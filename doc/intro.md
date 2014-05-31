f1aef2404e11361a9bf6b2dff597732281094225
Justin Campbell <justin@justincampbell.me>
2014-05-31 11:10:01 -0400

    lein new wwigt

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wwigt
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/wwigt/core_test.clj
@@ -0,0 +1,7 @@
+(ns wwigt.core-test
+  (:require [clojure.test :refer :all]
+            [wwigt.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
