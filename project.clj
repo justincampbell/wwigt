8a71250758a446b01829e29a23ff8e1597db8104
Justin Campbell <justin@justincampbell.me>
2014-06-04 16:25:08 -0400

    Add basic compojure skeleton

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
 (defproject wwigt "0.1.0-SNAPSHOT"
   :description "What was I going to ..."
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.6"]]
+  :plugins [[lein-ring "0.8.9"]]
+  :ring {:handler wwigt.handler/app}
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring-mock "0.1.5"]]}})
---
+++ test/wwigt/handler_test.clj
@@ -0,0 +1,14 @@
+(ns wwigt.handler-test
+  (:use clojure.test
+        ring.mock.request
+        wwigt.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
