7e8c9d4bd68322e80081167860e59cd6308442ad
Justin Campbell <justin@justincampbell.me>
2014-06-07 15:23:50 -0400

    Remove faiiling example test

--- test/wwigt/core_test.clj
+++ test/wwigt/core_test.clj
@@ -1,7 +1,4 @@
 (ns wwigt.core-test
   (:require [clojure.test :refer :all]
-            [wwigt.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+            [wwigt.core :refer :all]
+            :reload-all))
