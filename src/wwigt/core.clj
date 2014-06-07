687323f60903dd8bb0bf29306c797bf9b8f8e653
Justin Campbell <justin@justincampbell.me>
2014-06-07 16:55:00 -0400

    Fake url, generate path from url

--- src/wwigt/core.clj
+++ src/wwigt/core.clj
@@ -1,1 +1,16 @@
-(ns wwigt.core)
+(ns wwigt.core
+  (:use [clojure.string :only [join split]]))
+
+(defn get-random-article
+  "Gets a random article from Wikipedia"
+  []
+  "http://en.wikipedia.org/wiki/Sharks")
+
+(defn path-from-url
+  [url]
+  (str "/" (join "/" (drop 3 (split url #"/")))))
+
+(defn fetch []
+  (let [url (get-random-article)]
+    {:url url
+     :path (path-from-url url)}))
--- test/wwigt/core_test.clj
+++ test/wwigt/core_test.clj
@@ -2,3 +2,19 @@
   (:require [clojure.test :refer :all]
             [wwigt.core :refer :all]
             :reload-all))
+
+(def path "/wiki/Sharks")
+(def url "http://en.wikipedia.org/wiki/Sharks")
+
+(deftest fetch-test
+  (testing "returns a map with the required attributes"
+    (is (string? (:path (fetch))))
+    (is (string? (:url (fetch))))))
+
+(deftest get-random-article-test
+  (testing "returns a url from provider"
+    (is (= url (get-random-article)))))
+
+(deftest path-from-url-test
+  (testing "returns the path from a url"
+   (is (= path (path-from-url url)))))
