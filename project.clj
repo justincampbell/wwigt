a1a261bbc3119a01a60b9677bc4c6efbd72d4f15
Justin Campbell <justin@justincampbell.me>
2014-12-03 20:49:49 -0500

    Deploy to Heroku

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject wwigt "0.1.0-SNAPSHOT"
   :description "What was I going to ..."
   :url "http://example.com/FIXME"
+  :min-lein-version  "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]
                  [clj-http "0.9.2"]
-                 [hiccup "1.0.5"]]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.5"]
+                 [javax.servlet/servlet-api "2.5"]]
   :plugins [[lein-ring "0.8.9"]]
   :ring {:handler wwigt.handler/app}
-  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [ring-mock "0.1.5"]]}})
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
